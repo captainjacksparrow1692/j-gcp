51b4af496d6cc5513c17757164ee337f934f4bf0
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2025-12-17 23:39:16 +0500

    first commit

---
+++ src/main/java/uzumtech/j_gcp/JGcpApplication.java
@@ -0,0 +1,13 @@
+package uzumtech.j_gcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JGcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uzumtech/j_gcp/JGcpApplicationTests.java
@@ -0,0 +1,13 @@
+package uzumtech.j_gcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JGcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
