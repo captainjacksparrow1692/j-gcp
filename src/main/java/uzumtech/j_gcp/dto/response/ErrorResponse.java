b3523248fd7e11f79a1b37a082e4bd9711144b5a
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2025-12-22 00:43:35 +0500

    create ErrorResponse record

--- src/main/java/uzumtech/j_gcp/dto/response/ErrorResponse.java
+++ src/main/java/uzumtech/j_gcp/dto/response/ErrorResponse.java
@@ -7,8 +7,15 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 public class ErrorResponse {
+    //Время возникновения ошибки
     private LocalDateTime timestamp;
+
+    //Сообщение об ошибке
     private String message;
+
+    //Дополнительные детали ошибки
     private String details;
+
+    //HTTP статус-код
     private int statusCode;
 }
