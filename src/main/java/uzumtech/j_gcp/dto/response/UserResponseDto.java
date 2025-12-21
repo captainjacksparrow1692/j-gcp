39c18054872479d681d3e5b545637722c0fe82f3
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2025-12-22 00:38:45 +0500

    create UserResponseDto record

--- src/main/java/uzumtech/j_gcp/dto/response/UserResponseDto.java
+++ src/main/java/uzumtech/j_gcp/dto/response/UserResponseDto.java
@@ -10,20 +10,32 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserResponseDto {
+    //id пользователя
     private Long id;
+    //ФИО
     private String fullName;
+    //адрес
     private String address;
+    //почта
     private String email;
+    //тел.номер
     private String phoneNumber;
+    //ссылка на фото
     private String photoUrl;
+    //возраст
     private Integer age;
+    //пинфл
     private String pinfl;
+    //тип документа
     private DocumentType documentType;
+    //дата выдачи
     private LocalDate issueDate;
+    //срок истечения документа
     private LocalDate expiryDate;
+    //гражданство
     private String citizenship;
+    //дата смерти
     private LocalDate deathDate; // null, если пользователь жив
-
     // Вспомогательное поле для фронтенда
     public boolean isAlive() {
         return deathDate == null;
