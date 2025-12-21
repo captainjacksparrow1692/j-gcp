c71fc4c428f7a999b102bcbe4c7e918a93979c1a
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2025-12-22 00:19:54 +0500

    create User entity record

--- src/main/java/uzumtech/j_gcp/entity/User.java
+++ src/main/java/uzumtech/j_gcp/entity/User.java
@@ -15,44 +15,57 @@ import java.time.LocalDate;
 @Builder
 
 public class User {
+    //создание айди
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    //ФИО
     @Column(nullable = false)
     private String fullName;
 
+    //Адресс
     @Column(nullable = false)
     private String address;
 
+    //уникальная почта
     @Column(nullable = false,  unique = true)
     private String email;
 
+    //тел. номер(string потому что никаких операций не проводиться, просто храним номер)
     @Column(nullable = false)
     private String phoneNumber;
 
+    //ссылка на фото
     @Column(name = "photo_url")
     private String photoUrl;
 
+    //возраст
     @Column(nullable = false)
     private Integer age;
 
+    //пинфл должно быть минимум и максимум 14 знаков
     @Column(nullable = false,   unique = true, length = 14)
     private String pinfl;
 
+    //тип документа(айди,паспорт, метрка, права)
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private DocumentType documentType;
 
+    //дата выдачи
     @Column(nullable = false)
     private LocalDate issueDate;
 
+    //срок истечения
     @Column(nullable = false)
     private LocalDate expiryDate;
 
+    //гражданство
     @Column(nullable = false)
     private String citizenship;
 
+    //дата смерти
     @Column(name = "death_date")
     private LocalDate deathDate;
 }
