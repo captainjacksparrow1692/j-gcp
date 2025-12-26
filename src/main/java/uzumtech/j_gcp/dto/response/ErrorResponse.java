610c262fccdb87c85a36af36fe7fed0d6fb596d7
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2025-12-26 22:17:49 +0500

    ErrorResponse update

--- src/main/java/uzumtech/j_gcp/dto/response/ErrorResponse.java
+++ src/main/java/uzumtech/j_gcp/dto/response/ErrorResponse.java
@@ -1,23 +1,13 @@
 package uzumtech.j_gcp.dto.response;
 
 import lombok.Builder;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Getter
 @Builder
-public class ErrorResponse {
-
-    // Время возникновения ошибки
-    private final LocalDateTime timestamp;
-
-    // Сообщение об ошибке
-    private final String message;
-
-    // Дополнительные детали ошибки
-    private final String details;
-
-    // HTTP статус-код
-    private final int statusCode;
-}
+public record ErrorResponse(
+        LocalDateTime timestamp,
+        String message,
+        String details,
+        int statusCode
+) {}
