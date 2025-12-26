a3c7970011f7561e30ba6cf0cac854b5b486ce25
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2025-12-26 22:14:48 +0500

    MarkDeadResponseDto update

--- src/main/java/uzumtech/j_gcp/dto/response/MarkDeadResponseDto.java
+++ src/main/java/uzumtech/j_gcp/dto/response/MarkDeadResponseDto.java
@@ -5,17 +5,11 @@ import uzumtech.j_gcp.constant.enums.LifeStatus;
 
 import java.time.LocalDate;
 
-@Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class MarkDeadResponseDto {
-    //id пользователя
-    private Long userId;
-    //пинфл
-    private String pinfl;
-    //дата смерти
-    private LocalDate deathDate;
-    //статус операции
-    private LifeStatus status;
+public record MarkDeadResponseDto(
+        Long userId,
+        String pinfl,
+        LocalDate deathDate,
+        LifeStatus status
+) {
 }
