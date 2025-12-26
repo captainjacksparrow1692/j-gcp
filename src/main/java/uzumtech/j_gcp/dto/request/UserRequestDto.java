cce5b355e736cc3f8d489d0432c7d0ecc9641b92
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2025-12-26 22:30:37 +0500

    UserRequestDto update

--- src/main/java/uzumtech/j_gcp/dto/request/UserRequestDto.java
+++ src/main/java/uzumtech/j_gcp/dto/request/UserRequestDto.java
@@ -7,66 +7,52 @@ import uzumtech.j_gcp.constant.enums.Gender;
 
 import java.time.LocalDate;
 
-@Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserRequestDto {
-    //переписать через record
-    // ФИО пользователя
-    @NotBlank(message = "Имя обязательно")
-    private String fullName;
+public record UserRequestDto(
+        @NotBlank(message = "Имя обязательно")
+        String fullName,
 
-    // Пол(MALE, FEMALE)
-    private Gender gender;
+        @NotNull(message = "Gender is required")
+        Gender gender,
 
-    // Адрес проживания
-    @NotBlank(message = "Адрес обязателен")
-    private String address;
+        @NotBlank(message = "Адрес обязателен")
+        String address,
 
-    // Email пользователя
-    @Email(message = "Некорректный email")
-    @NotBlank(message = "Email обязателен")
-    private String email;
+        @Email(message = "Некорректный email")
+        @NotBlank(message = "Email обязателен")
+        String email,
 
-    // Номер телефона (международный формат)
-    @NotBlank(message = "Номер телефона обязателен")
-    @Pattern(
-            regexp = "^\\+\\d{9,15}$",
-            message = "Номер телефона должен быть в международном формате"
-    )
-    private String phoneNumber;
+        @NotBlank(message = "Номер телефона обязателен")
+        @Pattern(
+                regexp = "^\\+\\d{9,15}$",
+                message = "Номер телефона должен быть в международном формате"
+        )
+        String phoneNumber,
 
-    // Ссылка на фотографию
-    private String photoUrl;
+        @NotBlank(message = "Photo_URL обязателен")
+        String photoUrl,
 
-    // Возраст пользователя
-    @Min(value = 0, message = "Возраст не может быть отрицательным")
-    @Max(value = 150, message = "Возраст не может быть больше 150")
-    private Integer age;
+        @NotBlank(message = "Возраст обязателен")
+        @Min(value = 0, message = "Возраст не может быть отрицательным")
+        @Max(value = 150, message = "Возраст не может быть больше 150")
+        Integer age,
 
-    // ПИНФЛ (14 символов)
-    @Size(min = 14, max = 14, message = "ПИНФЛ должен состоять из 14 символов")
-    @NotBlank(message = "ПИНФЛ обязателен")
-    private String pinfl;
+        @Size(min = 14, max = 14, message = "ПИНФЛ должен состоять из 14 символов")
+        @NotBlank(message = "ПИНФЛ обязателен")
+        String pinfl,
 
-    // Тип документа
-    @NotNull(message = "Тип документа обязателен")
-    private DocumentType documentType;
+        @NotNull(message = "Тип документа обязателен")
+        DocumentType documentType,
 
-    // Дата выдачи документа
-    @PastOrPresent(message = "Дата выдачи не может быть в будущем")
-    private LocalDate issueDate;
+        @PastOrPresent(message = "Дата выдачи не может быть в будущем")
+        LocalDate issueDate,
 
-    // Дата окончания действия документа
-    @Future(message = "Срок действия документа должен быть в будущем")
-    private LocalDate expiryDate;
+        @Future(message = "Срок действия документа должен быть в будущем")
+        LocalDate expiryDate,
 
-    // Гражданство
-    @NotBlank(message = "Гражданство обязательно")
-    private String citizenship;
+        @NotBlank(message = "Гражданство обязательно")
+        String citizenship,
+        @PastOrPresent(message = "Дата смерти должна быть в прошлом или настоящем")
+        LocalDate deathDate
 
-    // Дата смерти (если есть)
-    @PastOrPresent(message = "Дата смерти должна быть в прошлом или настоящем")
-    private LocalDate deathDate;
-}
+) {}
