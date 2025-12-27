2e5191d1a86e17c4b045f54e6168130dd446880a
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2025-12-28 02:39:49 +0500

    update UserController

--- src/main/java/uzumtech/j_gcp/controller/UserController.java
+++ src/main/java/uzumtech/j_gcp/controller/UserController.java
@@ -1,128 +1,75 @@
 package uzumtech.j_gcp.controller;
 
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import uzumtech.j_gcp.constant.enums.DocumentType;
 import uzumtech.j_gcp.dto.request.MarkDeadRequestDto;
 import uzumtech.j_gcp.dto.request.UserRequestDto;
 import uzumtech.j_gcp.dto.response.MarkDeadResponseDto;
 import uzumtech.j_gcp.dto.response.UserResponseDto;
 import uzumtech.j_gcp.service.UserService;
 
-import jakarta.validation.Valid;
-import java.time.LocalDate;
-
+@Slf4j
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v1/users") // Добавили версию API v1 (хорошая практика)
 @RequiredArgsConstructor
 @Validated
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserController {
 
-    private final UserService userService;
+    UserService userService;
 
-    //CRUD
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public UserResponseDto createUser(@RequestBody @Valid UserRequestDto userRequestDto) {
-        return userService.createUser(userRequestDto);
-    }
-
-    @GetMapping
-    public Page<UserResponseDto> getAllUsers(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "100") int size) {
-
-        return userService.getAllUsers(PageRequest.of(page, size));
+    public ResponseEntity<UserResponseDto> createUser(@RequestBody @Valid UserRequestDto userRequestDto) {
+        log.info("REST request to save User : {}", userRequestDto.pinfl());
+        return new ResponseEntity<>(userService.createUser(userRequestDto), HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public UserResponseDto getUserById(@PathVariable Long id) {
-        return userService.getUserById(id);
+    public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id) {
+        log.debug("REST request to get User by id : {}", id);
+        return ResponseEntity.ok(userService.getUserById(id));
     }
 
     @GetMapping("/pinfl/{pinfl}")
-    public UserResponseDto getUserByPinfl(@PathVariable String pinfl) {
-        return userService.getUserByUserPinfl(pinfl);
+    public ResponseEntity<UserResponseDto> getUserByPinfl(@PathVariable String pinfl) {
+        log.debug("REST request to get User by PINFL : {}", pinfl);
+        return ResponseEntity.ok(userService.getUserByUserPinfl(pinfl));
     }
 
-    //Поиск
-    @GetMapping("/search")
-    //pageable переделка, пэйдж везьде должен быть одинаковым
-    public Page<UserResponseDto> searchUsersByName(@RequestParam String fullName, Pageable pageable) {
-        return userService.searchUsersByName(fullName, pageable);
-    }
-
-    @GetMapping("/alive")
-    public Page<UserResponseDto> getAllAliveUsers(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "100") int size) {
-
-        return userService.getAllAliveUsers(PageRequest.of(page, size));
-    }
-
-    @GetMapping("/dead")
-    public Page<UserResponseDto> getAllDeadUsers(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "100") int size) {
-
-        return userService.getAllDeadUsers(PageRequest.of(page, size));
-    }
-
-    //Статус жизни
-    @GetMapping("/{id}/alive")
-    public boolean isUserAlive(@PathVariable Long id) {
-        return userService.isUserAlive(id);
+    @GetMapping
+    public ResponseEntity<Page<UserResponseDto>> getAllUsers(@PageableDefault(size = 20) Pageable pageable) {
+        log.debug("REST request to get all Users");
+        return ResponseEntity.ok(userService.getAllUsers(pageable));
     }
 
     @PostMapping("/{id}/mark-dead")
-    public MarkDeadResponseDto markUserAsDead(
+    public ResponseEntity<MarkDeadResponseDto> markUserAsDead(
             @PathVariable Long id,
             @RequestBody @Valid MarkDeadRequestDto request) {
-        LocalDate deathDate = request.deathDate();
-        return userService.markUserAsDead(id, deathDate);
+        log.info("REST request to mark User as dead, id: {}", id);
+        var deathDate = request.deathDate();
+        return ResponseEntity.ok(userService.markUserAsDead(id, deathDate));
     }
 
-    //Статистика
     @GetMapping("/count")
-    public long getUsersCountByStatus(@RequestParam UserService.Status status) {
-        return userService.getUsersCountByStatus(status);
+    public ResponseEntity<Long> getUsersCountByStatus(@RequestParam UserService.Status status) {
+        log.debug("REST request to get users count by status: {}", status);
+        return ResponseEntity.ok(userService.getUsersCountByStatus(status));
     }
 
-    // Документы
     @GetMapping("/documents/expired")
-    public Page<UserResponseDto> getUsersWithExpiredDocuments(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "100") int size) {
-        return userService.getUsersWithExpiredDocuments(PageRequest.of(page, size));
-    }
-
-    @GetMapping("/documents/expiring")
-    public Page<UserResponseDto> getUsersWithDocumentsExpiringBetween(
-            @RequestParam LocalDate start,
-            @RequestParam LocalDate end,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "100") int size) {
-        return userService.getUsersWithDocumentsExpiringBetween(start, end, PageRequest.of(page, size));
-    }
-
-    @GetMapping("/documents/type/{type}")
-    public Page<UserResponseDto> getUsersByDocumentType(
-            @PathVariable DocumentType type,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "100") int size) {
-        return userService.getUsersByDocumentType(type, PageRequest.of(page, size));
-    }
-
-    // Сложные запросы
-    @GetMapping("/alive/expired-documents")
-    public Page<UserResponseDto> getAliveUsersWithExpiredDocuments(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "100") int size) {
-        return userService.getAliveUsersWithExpiredDocuments(PageRequest.of(page, size));
+    public ResponseEntity<Page<UserResponseDto>> getExpired(@PageableDefault(size = 20) Pageable pageable) {
+        log.debug("REST request to get users with expired documents");
+        return ResponseEntity.ok(userService.getUsersWithExpiredDocuments(pageable));
     }
 }
