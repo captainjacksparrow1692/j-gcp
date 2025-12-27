13b8727bfa2ef3263cee8a3c339029e9753e9bd9
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2025-12-28 03:03:23 +0500

    create RedisProps record

---
+++ src/main/java/uzumtech/j_gcp/configuration/props/RedisProps.java
@@ -0,0 +1,29 @@
+package uzumtech.j_gcp.configuration.props;
+
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+@ConfigurationProperties(prefix = "spring.data.redis")
+public class RedisProps {
+
+    String host;
+    int port;
+
+    int database;
+    String username;
+    String password;
+
+    int defaultTtl;
+
+    int timeout;
+    int shutdownTimeout;
+
+    int orderTtl;
+    int merchantTtl;
+}
+//взято из delivery
