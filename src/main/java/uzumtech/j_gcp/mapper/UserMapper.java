dddac467d1022835252a58026778f57648aea924
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2025-12-28 10:04:24 +0500

    update UserMapper

--- src/main/java/uzumtech/j_gcp/mapper/UserMapper.java
+++ src/main/java/uzumtech/j_gcp/mapper/UserMapper.java
@@ -11,20 +11,20 @@ import uzumtech.j_gcp.entity.User;
 @Mapper(componentModel = "spring", imports = {LifeStatus.class})
 public interface UserMapper {
 
-
     // 1. Создание сущности из Request DTO.
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "expirationDate", ignore = true)
     @Mapping(target = "status", ignore = true)
-    @Mapping(target = "lifeStatus",  ignore = true)
+    @Mapping(target = "lifeStatus", ignore = true)
+    @Mapping(target = "deathDate", ignore = true)
     User toEntity(UserRequestDto requestDto);
 
-
-    UserResponseDto toResponseDto(User user);
     // 2. Основной маппинг в ответ (Entity -> UserResponseDto).
-
+    UserResponseDto toResponseDto(User user);
 
     // 3. Маппинг для регистрации смерти (Entity -> MarkDeadResponseDto).
     @Mapping(target = "userId", source = "id")
+    // Используем тернарный оператор для определения статуса
     @Mapping(target = "status", expression = "java(user.getDeathDate() != null ? LifeStatus.DECEASED : LifeStatus.ALIVE)")
     MarkDeadResponseDto toMarkDeadResponseDto(User user);
 }
