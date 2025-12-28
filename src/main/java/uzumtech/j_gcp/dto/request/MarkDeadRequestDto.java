62bf8d2286b83b8aa5c20495a513c504fb866f50
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2025-12-29 01:16:14 +0500

    update MarkDeadRequestDto

--- src/main/java/uzumtech/j_gcp/dto/request/MarkDeadRequestDto.java
+++ src/main/java/uzumtech/j_gcp/dto/request/MarkDeadRequestDto.java
@@ -1,5 +1,6 @@
 package uzumtech.j_gcp.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.Builder;
@@ -10,6 +11,7 @@ import java.time.LocalDate;
 public record MarkDeadRequestDto (
 
         // Дата смерти пользователя
+        @JsonFormat(pattern = "yyyy-MM-dd")
         @NotNull(message = "Дата смерти обязательна")
         @PastOrPresent(message = "Дата смерти должна быть в прошлом или настоящем")
         LocalDate deathDate) {
