aef942db4806ff67b518fcb2c9acf4ca93b44aff
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2025-12-28 09:53:37 +0500

    update Constant

--- src/main/java/uzumtech/j_gcp/constant/Constant.java
+++ src/main/java/uzumtech/j_gcp/constant/Constant.java
@@ -7,15 +7,8 @@ public class Constant {
 
      // Ключи для кэширования в Redis
      public static final String USER_CACHE = "users";
-     public static final String USER_STATS_CACHE = "user_stats";
-
      // Названия топиков Kafka
      public static final String USER_TOPIC = "users_topic";
-     public static final String USER_DLT_TOPIC = "users_topic-DLT";
-
-     // Форматы дат (если понадобятся для кастомной сериализации)
-     public static final String DATE_FORMAT = "yyyy-MM-dd";
-
      // Сообщения об ошибках (опционально)
      public static final String USER_NOT_FOUND = "User not found with id: ";
 }
