a1b117cce8a20d7983e2d4b5b4fc7a52dcaadbbd
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2025-12-28 09:25:36 +0500

    update Constant

--- src/main/java/uzumtech/j_gcp/constant/Constant.java
+++ src/main/java/uzumtech/j_gcp/constant/Constant.java
@@ -5,7 +5,17 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Constant {
 
-     public static final String USER_NOT_FOUND = "User not found";
-     public static final String USER_REDIS_KEYS  = "Users";
-     public static final String MERCHANT_REDIS_KEYS = "merchants";
+     // Ключи для кэширования в Redis
+     public static final String USER_CACHE = "users";
+     public static final String USER_STATS_CACHE = "user_stats";
+
+     // Названия топиков Kafka
+     public static final String USER_TOPIC = "users_topic";
+     public static final String USER_DLT_TOPIC = "users_topic-DLT";
+
+     // Форматы дат (если понадобятся для кастомной сериализации)
+     public static final String DATE_FORMAT = "yyyy-MM-dd";
+
+     // Сообщения об ошибках (опционально)
+     public static final String USER_NOT_FOUND = "User not found with id: ";
 }
