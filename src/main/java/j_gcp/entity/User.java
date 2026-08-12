0f4c5ea6b156979be8e81d8643ac8064f1129064
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-08-12 19:06:55 +1000

    fix imports

--- src/main/java/j_gcp/entity/User.java
+++ src/main/java/j_gcp/entity/User.java
@@ -1,11 +1,21 @@
 package j_gcp.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
 import j_gcp.constant.enums.DocumentType;
 import j_gcp.constant.enums.Gender;
 import j_gcp.constant.enums.LifeStatus;
 import j_gcp.constant.enums.Status;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
